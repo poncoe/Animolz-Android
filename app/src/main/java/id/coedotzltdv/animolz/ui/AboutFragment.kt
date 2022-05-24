27ae77bc6ac8ed7318e20bffaf4e7c8b166f698d
poncoe <poncoe4mac@gmail.com>
2022-05-24 23:04:05 +0700

    improvment about

--- app/src/main/java/id/coedotzltdv/animolz/ui/AboutFragment.kt
+++ app/src/main/java/id/coedotzltdv/animolz/ui/AboutFragment.kt
@@ -1,10 +1,12 @@
 package id.coedotzltdv.animolz.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import id.coedotzltdv.animolz.R
 import id.coedotzltdv.animolz.databinding.FragmentAboutBinding
 
 class AboutFragment: Fragment() {
@@ -18,9 +20,18 @@ class AboutFragment: Fragment() {
         binding = FragmentAboutBinding.inflate(layoutInflater, container, false)
         setHasOptionsMenu(true)
         return binding.root
+    }
 
-        binding.btnSaran.setOnClickListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.btnShare.setOnClickListener {
+            val sharingIntent = Intent(Intent.ACTION_SEND)
+            sharingIntent.type = "text/plain"
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, requireActivity().getString(R.string.app_name))
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, "Animolz Aplikasi Hewan!\n\nUnduh Sekarang!\nhttp://play.google.com/store/apps/details?id=id.coedotzltdv.animolz")
+            requireActivity().startActivity(Intent.createChooser(sharingIntent, "Bagikan Melalui"))
         }
     }
+
 }
