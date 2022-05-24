d8ee61a68712d5cc6b0d55a895f6ed45b14c0e05
poncoe <poncoe4mac@gmail.com>
2022-05-25 00:58:28 +0700

    datastore

--- app/src/main/java/id/coedotzltdv/animolz/ui/PendataanFragment.kt
+++ app/src/main/java/id/coedotzltdv/animolz/ui/PendataanFragment.kt
@@ -10,11 +10,13 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import id.coedotzltdv.animolz.R
+import id.coedotzltdv.animolz.SettingDataStore
 import id.coedotzltdv.animolz.databinding.FragmentPendataanBinding
 
 class PendataanFragment : Fragment() {
 
     private lateinit var binding: FragmentPendataanBinding
+    private lateinit var layoutDataStore: SettingDataStore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
